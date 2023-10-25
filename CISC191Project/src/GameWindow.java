9bfc16a91bbfd51c9b7610f6356e0b696ec77bab
salkhaliefah <137141135+salkhaliefah@users.noreply.github.com>
2023-10-25 11:32:44 -0700

    Update GameWindow.java

---
+++ CISC191Project/src/GameWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+
+
+public class GameWindow extends JFrame
+{
+	final int WINDOW_WIDTH = 605;
+	final int WINDOW_HEIGHT = 500;
+	
+	
+}
+
